bfc207fe43d5549bf753f7d8826ea567a49c8cb3
Timka6434 <timka6434@mail.ru>
2024-07-19 17:13:34 +0500

    someone code

--- Program.cs
+++ Program.cs
@@ -2,11 +2,19 @@
 
 static void Main(string[] args)
 {
-    
-
-
-
+    string UserInput = args[0].ToLower();
     Console.WriteLine("Добро пожаловать в программу по управлению сотрудниками");
 
-
+    Console.WriteLine("" +
+        "Доступны следующие команды:\n" +
+        " 1) -add First Name Last Name Salary\n" +
+        " 2) -update ID (First Name/Last Name/Salary)\n" +
+        " 3) -delete ID \n" +
+        " 4) -get ID" +
+        " 5) -getall");
+    UserInput = Console.ReadLine();
+    switch (UserInput)
+        {
+        case "-add"
+    }
 }
